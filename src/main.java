81e6eff9d34fa58e7b8bde36d270fddb7bb0ccee
Agas Yanpratma <agas.yanpratama@gmail.com>
2018-08-11 18:12:30 +0700

    Add CyclicRotation

--- src/main.java
+++ src/main.java
@@ -1,4 +1,7 @@
 import A_Iterations.BinaryGap;
+import B_Arrays.CyclicRotation;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class main {
@@ -11,12 +14,21 @@ public class main {
             String input = scanner.nextLine();
             if (input.equals("binaryGap")) {
                 BinaryGap binaryGap = new BinaryGap();
-                int answer = binaryGap.solution(51712);
-                System.out.println(answer + "\n");
+                System.out.println("Enter a number for binary gap command :");
+                int inInt = scanner.nextInt();
+                int answer = binaryGap.solution(inInt);
+                System.out.println("Longest binary gap in " + inInt + " is " + answer + "\n");
             } else if (input.equals("list")) {
                 String list = "1. 'binaryGap' \n" +
-                        "2. 'end' \n";
+                        "2. 'cyclicRotation' \n" +
+                        "3. 'end' \n";
                 System.out.println(list);
+            } else if (input.equals("cyclicRotation")) {
+                CyclicRotation cyclicRotation = new CyclicRotation();
+                int[] A = new int[] {3, 8, 9, 7, 6};
+                int[] arrSol = cyclicRotation.solution(A,1);
+                String out = Arrays.toString(arrSol);
+                System.out.println(out);
             } else if (input.equals("end")){
                 check = false;
             } else {
